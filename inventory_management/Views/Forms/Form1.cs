90561a7cda2f9b17bdfa555bf20ac45fec811a17
Osama Yeslam <os7softgithub@gmail.com>
2023-10-14 12:05:07 +0300

    Upload file

---
+++ inventory_management/Views/Forms/Form1.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace inventory_management
+{
+    public partial class Form1 : DevExpress.XtraEditors.DirectXForm
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+            
+        }
+
+        private void xtraScrollableControl2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void xtraTabControl1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void xtraTabPage3_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void barButtonItem7_ItemClick(object sender, DevExpress.XtraBars.ItemClickEventArgs e)
+        {
+
+        }
+    }
+}
