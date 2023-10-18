82a2f2c537fed44fc8d6d0a8e385c6f3f10a6dbd
ale305450 <138280966+ale305450@users.noreply.github.com>
2023-10-19 00:37:04 +0300

    Adding inventory interface

---
+++ inventory_management/Views/Interfaces/IInventoryView.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace inventory_management.Views.Interfaces
+{
+    public interface IInventoryView
+    {
+        //fields
+        int InventoryId { get;}
+        string InventoryName { get; set; }
+        string InventoryLocation { get; set; }
+        string InventoryCapacity { get; set; }
+        int CategoryId { get; set; }
+        string Message { set; }
+        bool IsSuccessed {set; }
+        bool IsEdit { get; set; }
+
+        //event 
+        event EventHandler SaveEvent;
+        event EventHandler AddEvent;
+        event EventHandler EditEvent;
+        event EventHandler DeleteEvent;
+
+
+        //Get Data
+        BindingSource InventoryList { set; }
+
+    }
+}
