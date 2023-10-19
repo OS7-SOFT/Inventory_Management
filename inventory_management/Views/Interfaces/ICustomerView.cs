4ad286de044e30af810df9c8a764dac091a6d7a1
ale305450 <138280966+ale305450@users.noreply.github.com>
2023-10-19 20:26:11 +0300

    Adding customer and order clsses and interfaces

---
+++ inventory_management/Views/Interfaces/ICustomerView.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace inventory_management.Views.Interfaces
+{
+    public interface ICustomerView
+    {
+        //fields
+        int CustomerId { get; }
+        string CustomerName { get; set; }
+        int CustomerPhone { get; set; }
+        string CustomerEmail { get; set; }
+        string CustomerLocation { get; set; }
+        string Message { set; }
+        bool IsSuccessed { set; }
+        bool IsEdit { set; get; }
+
+        //event 
+        event EventHandler SaveEvent;
+        event EventHandler AddEvent;
+        event EventHandler EditEvent;
+        event EventHandler DeleteEvent;
+
+
+        //Get Data
+        BindingSource CustomerList { set; }
+
+    }
+}
---
+++ inventory_management/Views/Interfaces/IOrderView.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace inventory_management.Views.Interfaces
+{
+    public interface IOrderView
+    {
+        //fields
+        int OrderId { get; }
+        int ProductId { get; set; }
+        int OrderedQuantity { get; set; }
+        DateTime OrderDate { set; }
+        string DeliveryStatus { get; set; }
+        int CustomerId { get; set; }
+        string Message { set; }
+        bool IsSuccessed { set; }
+        bool IsEdit { set; get; }
+
+        //event 
+        event EventHandler SaveEvent;
+        event EventHandler AddEvent;
+        event EventHandler EditEvent;
+        event EventHandler DeleteEvent;
+
+
+        //Get Data
+        BindingSource OrderList { set; }
+
+    }
+}
